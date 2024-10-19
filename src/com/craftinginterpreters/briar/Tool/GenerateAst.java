3cf1a7828076c3c5e0e0c1c102fa674d9363bf26
Kishon Daniels <kishondaniels@gmail.com>
2024-10-19 10:28:57 -0500

    Completed Chapt. 2, created visitor expressions

--- src/com/craftinginterpreters/briar/Tool/GenerateAst.java
+++ src/com/craftinginterpreters/briar/Tool/GenerateAst.java
@@ -30,6 +30,8 @@ public class GenerateAst {
             writer.println();
             writer.println("abstract class " +  baseName + " {");
 
+            defineVisitor(writer, baseName, types);
+
             // The Ast classes
             for (String type : types) {
                 String className = type.split(":")[0].trim();
@@ -37,9 +39,24 @@ public class GenerateAst {
                 defineType(writer, baseName, className, fields);
             }
 
+            // The base accept() method
+            writer.println();
+            writer.println(" abstract <R> R accept(Visitor<R> visitor);");
+
             writer.println("}");
             writer.close();
 
+        }
+
+        private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+            writer.println(" interface Visitor<R> {" );
+
+            for (String type : types) {
+                String typeName = type.split(":")[0].trim();
+                writer.println(" R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+            }
+
+            writer.println(" }");
         }
 
         private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
@@ -61,5 +78,13 @@ public class GenerateAst {
             }
 
             writer.println("  }");
+
+            // Visitor pattern
+            writer.println();
+            writer.println("    @Override");
+            writer.println("    <R> R accept(Visitor<R> visitor) {");
+            writer.println("    return visitor.visit" + className + baseName + "(this);");
+            writer.println("    }");
         }
+        
 }
